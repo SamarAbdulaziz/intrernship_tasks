54a19f71508502c1bb253a1439b4ac6f23f9ddf8
Samar <sam_abdulaziz@yahoo.com>
2022-06-28 03:39:48 +0200

    completing onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:internship_projects/OnBoardingScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,9 +9,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle(
+           statusBarColor: Colors.white,
+          )
+        )
+      ),
+      home: OnBoardingScreen(),
+
+    );
   }
 }
