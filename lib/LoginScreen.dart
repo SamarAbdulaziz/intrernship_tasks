102cd6a1f0efe42028018e05ee511cf1598b8226
Samar <sam_abdulaziz@yahoo.com>
2022-06-29 03:35:02 +0200

    fixing scrolling issue

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -9,101 +9,102 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
               width: double.infinity,
+              height: 300.0,
               child: Image.asset(
                 'assets/images/background.jpg',
                 fit: BoxFit.cover,
               ),
             ),
-          ),
-          const SizedBox(height: 20.0),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Welcome to fashion Daily',
-                ),
-                const SizedBox(height: 10.0),
-                mainRawWithHelp(screenTitle: 'Sign in'),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  'Phone Number',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.grey[800],
+            const SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Welcome to fashion Daily',
                   ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  children: [
-                    CountryCodePicker(
-                      initialSelection: 'eg',
-                      showFlag: false,
-                    ),
-                    Expanded(
-                      child: MyTextFormFeild(
-                        hint: '1023456789',
-                        keyboardtype: TextInputType.phone
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                myElevatedButton(text: 'Sign In'),
-                Image.asset('assets/images/or.png'),
-                signInByGoogleButton(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Doesn\'t has any account?',
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterScreen(),
-                          ),
-                        );
-                      },
-                      child: const Text('Register here'),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text(
-                  'use the application according to policy rules. '
-                  'Any kinds of violations will be subject to sanctions.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.grey,
+                  const SizedBox(height: 10.0),
+                  mainRawWithHelp(screenTitle: 'Sign in'),
+                  const SizedBox(
+                    height: 10.0,
                   ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-              ],
+                  Text(
+                    'Phone Number',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    children: [
+                      CountryCodePicker(
+                        initialSelection: 'eg',
+                        showFlag: false,
+                      ),
+                      Expanded(
+                        child: MyTextFormFeild(
+                          hint: '1023456789',
+                          keyboardtype: TextInputType.phone
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  myElevatedButton(text: 'Sign In'),
+                  Image.asset('assets/images/or.png'),
+                  signInByGoogleButton(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Doesn\'t has any account?',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text('Register here'),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  const Text(
+                    'use the application according to policy rules. '
+                    'Any kinds of violations will be subject to sanctions.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
