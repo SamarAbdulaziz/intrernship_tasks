51f2f5ffa74245336dfe40f4183003b79ad3de1e
Samar <sam_abdulaziz@yahoo.com>
2022-07-05 16:59:01 +0200

    last editing

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+
       builder: (context, child) => MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
